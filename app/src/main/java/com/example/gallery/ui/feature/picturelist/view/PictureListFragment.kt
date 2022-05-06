649967651ef7816d5b6dea99af4d5381cafed7c3
alexey-chaichuk <alexey.chaichuk@gmail.com>
2022-05-06 13:27:35 +0500

    Initial commit

---
+++ app/src/main/java/com/example/gallery/ui/feature/picturelist/view/PictureListFragment.kt
@@ -0,0 +1,12 @@
+package com.example.gallery.ui.feature.picturelist.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.gallery.R
+
+class PictureListFragment : Fragment(R.layout.fragment_picture_list) {
+
+}
